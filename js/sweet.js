ab5e147f8eb49659fe1cada6fc13def2e4184ccf
Helplyn <arianorvelinre@gmail.com>
2023-11-12 19:56:11 -0600

    SA estilos

--- js/sweet.js
+++ js/sweet.js
@@ -3,8 +3,94 @@ document.addEventListener("DOMContentLoaded", function () {
 
     htmlButton.addEventListener("click", function () {
         Swal.fire({
-            text: 'Llevo 5 años en el mundo de la programacion web, y todo comenzo con un pequeño bloc de notas que tenia en mi escritorio',
-            confirmButtonText: '¡Entendido!'
-        });
+            imageUrl: "./infoboton/html.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlButton = document.getElementById("boton2");
+
+    htmlButton.addEventListener("click", function () {
+        Swal.fire({
+            imageUrl: "./infoboton/css.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlButton = document.getElementById("boton3");
+
+    htmlButton.addEventListener("click", function () {
+        Swal.fire({
+            imageUrl: "./infoboton/js.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlButton = document.getElementById("boton4");
+
+    htmlButton.addEventListener("click", function () {
+        Swal.fire({
+            imageUrl: "./infoboton/boot.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlButton = document.getElementById("boton5");
+
+    htmlButton.addEventListener("click", function () {
+        Swal.fire({
+            imageUrl: "./infoboton/my.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var htmlButton = document.getElementById("boton6");
+
+    htmlButton.addEventListener("click", function () {
+        Swal.fire({
+            imageUrl: "./infoboton/canva.gif",
+            imageWidth: 1200,
+            imageHeight: null,
+            showConfirmButton: false,
+            background: "#000000",
+            backdrop: `
+              rgb(0, 0, 0, 0.5)
+            `,
+          });                            
     });
 });
